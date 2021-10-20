0f6168ad4ff1c4fc99c0759daa89033adc20aa1e
Bui Van Duc <90807599+BuiDuc1812@users.noreply.github.com>
2021-10-20 10:32:40 +0700

    Update Script.js

--- Assets/Js/Script.js
+++ Assets/Js/Script.js
@@ -8,14 +8,18 @@ backBtn = wrapper.querySelector('.fa-backward')
 toBtn = wrapper.querySelector('.fa-forward')
 progressBar = wrapper.querySelector('.progress-bar')
 progressArea = wrapper.querySelector('.progress-area')
+musicList = wrapper.querySelector('.music-list')
+showMoreBtn = wrapper.querySelector('.fa-list-ul')
+hideMoreBtn = musicList.querySelector('.fa-times')
 
 
 
 
-let musicIndex = 2;
+let musicIndex = Math.floor((Math.random()*allMusic.length)+1);
 
 window.addEventListener("load",()=>{
     loadMusic(musicIndex)
+    playingNow()
 })
 
 function loadMusic(indexNumb){
@@ -39,6 +43,7 @@ function pauseMusic(){
 playPauseBtn.addEventListener("click",()=>{
     const isMusicPaused = wrapper.classList.contains("paused")
     isMusicPaused ? pauseMusic() : playMusic();
+    playingNow()
 })
 
 function nextMusic(){
@@ -46,12 +51,16 @@ function nextMusic(){
     musicIndex>allMusic.length ? musicIndex = 1: musicIndex = musicIndex
     loadMusic(musicIndex);
     playMusic();
+    playingNow()
+
 }
 function downMusic(){
     musicIndex--;
     musicIndex < 1 ? musicIndex = allMusic.length : musicIndex = musicIndex
     loadMusic(musicIndex)
     playMusic();
+    playingNow()
+
 }
 toBtn.addEventListener('click',()=>{
     nextMusic();
@@ -100,9 +109,90 @@ progressArea.addEventListener("click",(e)=>{
 
 const repeatBtn = wrapper.querySelector('.fa-redo-alt')
  repeatBtn.addEventListener("click", ()=>{
-    let getText = repeatBtn.innerText
-    switch(getText){
-        case "repeat":
-            repeatBtn.innerText = "repeat_one"
+    let getText = repeatBtn.classList
+    if(getText == 'fas fa-redo-alt'){
+        repeatBtn.classList ='fas fa-undo'
+    }
+    else if(getText == 'fas fa-undo'){
+        repeatBtn.classList = 'fas fa-redo-alt'
     }
  })
+
+
+//  sau khi bài hát kết thúc
+
+mainAudio.addEventListener("ended",()=>{
+    let getText = repeatBtn.classList
+    if(getText == 'fas fa-redo-alt'){
+        nextMusic();
+    }
+    else if(getText == 'fas fa-undo'){
+        mainAudio.currentTime = 0
+        loadMusic(musicIndex)
+        playMusic()
+    }
+})
+
+showMoreBtn.addEventListener("click",()=>{
+    musicList.classList.toggle("show")
+})
+hideMoreBtn.addEventListener("click",()=>{
+    musicList.classList.remove("show")
+})
+
+
+
+const ulTag = wrapper.querySelector('ul')
+for(let i = 0 ; i <allMusic.length; i++){
+    let liTag= `<li li-index="${i + 1}">
+                <div class="row">
+                    <span>${allMusic[i].name}</span>
+                    <p>${allMusic[i].artist}</p>
+                </div>
+                <audio class="${allMusic[i].src}" src="./Assets/Music/${allMusic[i].src}.mp3"></audio>
+                <span id ="${allMusic[i].src}" class="audio-duration">3:40</span>
+                </li>`;
+
+    ulTag.insertAdjacentHTML("beforeend",liTag);
+
+    let liAudioDuaration = ulTag.querySelector(`#${allMusic[i].src}`)
+    let liAudioTag = ulTag.querySelector(`.${allMusic[i].src}`)
+
+    liAudioTag.addEventListener("loadeddata",()=>{
+        let audioDuration = liAudioTag.duration;
+            totalMin = Math.floor(audioDuration / 60)
+            totalSec = Math.floor(audioDuration % 60)
+        if(totalSec < 10){
+            totalSec =`0${totalSec}`
+        }
+        liAudioDuaration.innerText = `${totalMin}:${totalSec}`;   
+        liAudioDuaration.setAttribute("t-duration", `${totalMin}:${totalSec}`)
+    })
+}
+
+
+const allLiTags = ulTag.querySelectorAll("li")
+
+ function playingNow(){
+    for(let j = 0; j< allLiTags.length; j++){
+        let audioTag = allLiTags[j].querySelector(".audio-duration")
+        if(allLiTags[j].classList.contains("playing")){
+            allLiTags[j].classList.remove("playing")
+            let adDuration = audioTag.getAttribute("t-duration")
+            audioTag.innerText = adDuration
+        }
+        if(allLiTags[j].getAttribute("li-index") == musicIndex){
+           allLiTags[j].classList.add("playing")
+           audioTag.innerText = "Playing"
+        }
+        allLiTags[j].setAttribute("onclick", "clicked(this)")
+    }
+ }
+
+ function clicked(element){
+     let getliIndex = element.getAttribute("li-index")
+     musicIndex = getliIndex
+     loadMusic(musicIndex)
+     playMusic()
+     playingNow()
+ }
